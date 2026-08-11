fb7c6f5152e1e33e9cb41b1545bf26d0cb641bc0
Dan Seban <dan.seban@edu.esiee.fr>
2026-08-11 15:50:40 +0200

    fix: user creation colision

--- codabench_loadtest/models/users.py
+++ codabench_loadtest/models/users.py
@@ -4,12 +4,15 @@ import random
 
 from faker import Faker
 from pydantic import BaseModel, Field
+import uuid
 
 
 class User(BaseModel):
-    username: str = Field(default_factory=lambda: Faker().user_name())
+    username: str = Field(default_factory=lambda: f"{Faker().user_name()}{uuid.uuid4().hex[:8]}")
     password: str = Field(default_factory=lambda: Faker().password())
-    email: str = Field(default_factory=lambda: Faker().email())
+    email: str = Field(
+        default_factory=lambda: f"{Faker().user_name()}{uuid.uuid4().hex[:8]}@{Faker().free_email_domain()}"
+    )
     id: str | None = Field(default=None, description="User ID assigned by the server")
 
 
